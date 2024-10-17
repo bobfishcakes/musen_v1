ff3c092100de5a4d7688fc5f4ec92efb1c7e6a48
Marblism CLI <studio@marblism.com>
2024-10-17 23:00:16 +0000

    Session saved

--- apps/server/src/core/cors/cors.service.ts
+++ apps/server/src/core/cors/cors.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common'
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 import {
   ConfigurationService,
   ConfigurationServiceObject,
@@ -11,25 +10,17 @@ export class CorsService {
 
   getOptions() {
     const clientBaseUrl = this.configurationService.getClientBaseUrl()
-
-    const options: Record<ConfigurationServiceObject.Environment, CorsOptions> =
-      {
-        [ConfigurationServiceObject.Environment.DEVELOPMENT]: {
-          origin: [clientBaseUrl],
-          credentials: true,
-        },
-        [ConfigurationServiceObject.Environment.PRODUCTION]: {
-          origin: clientBaseUrl,
-          credentials: true,
-        },
-      }
-
     const environment = this.configurationService.getEnvironment()
 
-    const value = options[environment]
-    const valueDefault =
-      options[ConfigurationServiceObject.Environment.DEVELOPMENT]
+    const options = {
+      origin:
+        environment === ConfigurationServiceObject.Environment.PRODUCTION
+          ? clientBaseUrl
+          : [clientBaseUrl],
+      credentials: true,
+    }
 
-    return value ?? valueDefault
+    console.log('CORS options:', options)
+    return options
   }
 }
